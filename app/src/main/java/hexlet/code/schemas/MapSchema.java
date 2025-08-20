839d178f189f241f8f8adeac88c97415bd2ffc81
Anasstasiya Sviridenkova <anastasia_sviridenkova@mail.ru>
2025-08-20 23:43:03 +0500

    add class MapSchema

---
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -0,0 +1,15 @@
+package hexlet.code.schemas;
+
+import java.util.Map;
+
+public class MapSchema extends BaseSchema<Map<?, ?>>{
+    public MapSchema required() {
+        addCheck("required", (map) -> map != null); //map instanceof Map
+        return this;
+    }
+
+    public MapSchema sizeof(int length) {
+        addCheck("sizeof", (map) -> map != null && map.size() == length);
+        return this;
+    }
+}
